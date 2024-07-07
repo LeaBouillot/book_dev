08c947905e180ea5defc413f0e61210a2203c81d
bestdevelope <leabouillot7@gmail.com>
2024-07-07 17:01:49 +0200

    skill

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 function Skill() {
-  return <div>Skill</div>;
+  return (
+    <section id="skill">
+      <div className="skill__inner">
+        <h2 className="skill__title">Challenge</h2>
+        <div>
+          <div>
+            <span>1.</span>
+            <h3>
+              En cours: TP Concepteur Développeur d'Applications de niveau 6 à
+              PREPAVENIR FORMATION France
+            </h3>
+            <p>
+              Compétences en cours d'acquisition : JavaScript · React · Node.js
+              · PHP · SQL · Framework Symfony
+            </p>
+          </div>
+
+          <div>
+            <span>2.</span>
+            <h3>Titre Profesionelle RNCP Graphiste & Web Designer</h3>
+            <p>
+              Figma, Adobe Creative Suite <br />
+              Design d’interface utilisateur <br />
+              WordPress Developer
+            </p>
+          </div>
+
+          <div>
+            <span>2.</span>
+            <h3>Motivation</h3>
+            <p>
+              Je suis très motivée et impatiente de mettre en pratique mes
+              connaissances dans un environnement professionnel stimulant. Je
+              suis convaincue que mon profil et mon enthousiasme pour le
+              développement d'applications pourront apporter une valeur ajoutée
+              à votre équipe.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Skill;
