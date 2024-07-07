b394841b74aa461889be2e03bd796e74c577d7ce
bestdevelope <leabouillot7@gmail.com>
2024-07-07 17:15:07 +0200

    skill-title-desc

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,44 +1,35 @@
 import React from "react";
 
+const skillText = [
+  {
+    title:
+      "En cours: TP Concepteur Développeur d'Applications de niveau 6 à PREPAVENIR FORMATION France",
+    desc: "Compétences en cours d'acquisition : JavaScript · React · Node.js · PHP · SQL · Framework Symfony",
+  },
+  {
+    title: "Titre Profesionelle RNCP Graphiste & Web Designer",
+    desc: "Figma, Adobe Creative Suite. Design d’interface utilisateur. WordPress Developer",
+  },
+  {
+    title: "Mes motivations",
+    desc: "Je suis très motivée et impatiente de mettre en pratique mes connaissances dans un environnement professionnel stimulant à votre équipe.",
+  },
+];
+
 function Skill() {
   return (
     <section id="skill">
       <div className="skill__inner">
         <h2 className="skill__title">Challenge</h2>
-        <div>
-          <div>
-            <span>1.</span>
-            <h3>
-              En cours: TP Concepteur Développeur d'Applications de niveau 6 à
-              PREPAVENIR FORMATION France
-            </h3>
-            <p>
-              Compétences en cours d'acquisition : JavaScript · React · Node.js
-              · PHP · SQL · Framework Symfony
-            </p>
-          </div>
 
-          <div>
-            <span>2.</span>
-            <h3>Titre Profesionelle RNCP Graphiste & Web Designer</h3>
-            <p>
-              Figma, Adobe Creative Suite <br />
-              Design d’interface utilisateur <br />
-              WordPress Developer
-            </p>
-          </div>
-
-          <div>
-            <span>2.</span>
-            <h3>Motivation</h3>
-            <p>
-              Je suis très motivée et impatiente de mettre en pratique mes
-              connaissances dans un environnement professionnel stimulant. Je
-              suis convaincue que mon profil et mon enthousiasme pour le
-              développement d'applications pourront apporter une valeur ajoutée
-              à votre équipe.
-            </p>
-          </div>
+        <div className="skill__desc">
+          {skillText.map((skill, key) => (
+            <div key={key}>
+              <span>{key + 1}.</span>
+              <h3>{skill.title}</h3>
+              <p>{skill.desc}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
