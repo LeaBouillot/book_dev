e3faa16d39918e135a185c8c5f18c8559f267757
bestdevelope <leabouillot7@gmail.com>
2024-07-07 18:35:01 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,57 @@
 import React from "react";
 
+const contactText = [
+  {
+    link: "https://my-site.com/contact",
+    title: "MY site",
+  },
+  {
+    link: "mailto: leabouillot7@gmail.com",
+    title: "mail: leabouillot",
+  },
+];
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contactez-moi</h2>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            {contactText.map((contact, key) => (
+              <div key={key}>
+                <a
+                  href={contact.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {contact.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
