92af0f236f99f7f29127d182bfb3c2ef158cc7d7
bestdevelope <leabouillot7@gmail.com>
2024-07-07 17:56:27 +0200

    site

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,7 +1,95 @@
 import React from "react";
 
+const siteText = [
+  {
+    text: ["make", "site compliant with", "react.js"],
+    title: "Site web with React",
+    code: "https://github.com/bestdevelope/my-portfolio",
+    view: "https://react.netlify.app",
+    info: [
+      "site coding",
+      "production period : two days",
+      "use stack : HTML5/CSS3, CSS Variable, Vite",
+    ],
+  },
+  {
+    text: ["make", "site compliant with", "react.js"],
+    title: "Site web with React",
+    code: "https://github.com/bestdevelope/my-portfolio",
+    view: "https://react.netlify.app",
+    info: [
+      "site coding",
+      "production period : two days",
+      "use stack : HTML5/CSS3, CSS Variable, Vite",
+    ],
+  },
+  {
+    text: ["make", "site compliant with", "react.js"],
+    title: "Site web with React",
+    code: "https://github.com/bestdevelope/my-portfolio",
+    view: "https://react.netlify.app",
+    info: [
+      "site coding",
+      "production period : two days",
+      "use stack : HTML5/CSS3, CSS Variable, Vite",
+    ],
+  },
+  {
+    text: ["make", "site compliant with", "react.js"],
+    title: "Site web with React",
+    code: "https://github.com/bestdevelope/my-portfolio",
+    view: "https://react.netlify.app",
+    info: [
+      "site coding",
+      "production period : two days",
+      "use stack : HTML5/CSS3, CSS Variable, Vite",
+    ],
+  },
+  {
+    text: ["make", "site compliant with", "react.js"],
+    title: "Site web with React",
+    code: "https://github.com/bestdevelope/my-portfolio",
+    view: "https://react.netlify.app",
+    info: [
+      "site coding",
+      "production period : two days",
+      "use stack : HTML5/CSS3, CSS Variable, Vite",
+    ],
+  },
+];
+
 function Site() {
-  return <div>Site</div>;
+  return (
+    <section className="site">
+      <div className="site__inner">
+        <h2 className="site__title">
+          Site coding <em>Projets</em>
+        </h2>
+        <div className="site__wrap">
+          {siteText.map((site, key) => (
+            <article className={`site__item s${key + 1}`} key={key}>
+              <span className="num">{key + 1}.</span>
+              <div className="text">
+                <div>{site.text[0]}</div>
+                <div>{site.text[1]}</div>
+                <div>{site.text[2]}</div>
+              </div>
+              <h3 className="title">{site.title}</h3>
+              <div className="btn">
+                <a href={site.code}>code</a>
+                <a href={site.view}>site</a>
+              </div>
+              <div className="info">
+                <span>{site.info[0]}</span>
+                <span>{site.info[1]}</span>
+                <span>{site.info[2]}</span>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Site;
