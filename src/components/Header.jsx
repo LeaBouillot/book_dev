963872edd276b4b396a32e6fea5c5f63a8a4a3d6
bestdevelope <leabouillot7@gmail.com>
2024-07-07 12:13:29 +0200

    skip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header id="header" role="banner">
+      <div className="header__inner">
+        <div className="header__logo">
+          <h1>
+            <a href="/">
+              portfolio<em>React</em>
+            </a>
+          </h1>
+        </div>
+        <nav className="header__nav" role="navigation" aria-label="main menu">
+          <ul>
+            <li>
+              <a href="#intro">intro</a>
+            </li>
+            <li>
+              <a href="#skill">skill</a>
+            </li>
+            <li>
+              <a href="#site">site</a>
+            </li>
+            <li>
+              <a href="#portfolio">portfolio</a>
+            </li>
+            <li>
+              <a href="#contact">contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div
+          className="header__nav__mobile"
+          id="headerToggle"
+          aria-controls="primary-menu"
+          aria-expanded="false"
+          role="button"
+          tabindex="0"
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Skip() {
-  return <div>Skip</div>;
+  return (
+    <div id="skip">
+      <a href="#header">Aller vers le header</a>
+      <a href="#intro">Aller vers l'intro</a>
+      <a href="#skill">Aller vers le skill</a>
+      <a href="#site">Aller vers le site</a>
+      <a href="#port">Aller vers le portfolio</a>
+      <a href="#contact">Aller vers le contact</a>
+      <a href="#footer">Aller vers le footer</a>
+    </div>
+  );
 }
 
 export default Skip;
